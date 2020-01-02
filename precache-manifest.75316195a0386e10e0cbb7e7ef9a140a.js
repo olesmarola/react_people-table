087331eae9a17be6060c38771f0cb9b9cfd81db2
Oles Marola <oles_pw@ukr.net>
2020-01-02 17:55:12 +0200

    Updates

--- precache-manifest.75316195a0386e10e0cbb7e7ef9a140a.js
+++ precache-manifest.75316195a0386e10e0cbb7e7ef9a140a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "707fb987a2f9bcfef550f2a338b4ee71",
+    "revision": "451e34b9a6cb660a1f0a4c4a940a6a47",
     "url": "/react_people-table/index.html"
   },
   {
-    "revision": "89e6bf9e98e174925b46",
+    "revision": "e5c3bca0c08661dac485",
     "url": "/react_people-table/static/css/main.dc6b6f49.chunk.css"
   },
   {
-    "revision": "b7b8e5007f301887e57b",
-    "url": "/react_people-table/static/js/2.f66401a1.chunk.js"
+    "revision": "80aa8937172682e22d38",
+    "url": "/react_people-table/static/js/2.cff26664.chunk.js"
   },
   {
-    "revision": "89e6bf9e98e174925b46",
-    "url": "/react_people-table/static/js/main.a2428cdf.chunk.js"
+    "revision": "e5c3bca0c08661dac485",
+    "url": "/react_people-table/static/js/main.789f7010.chunk.js"
   },
   {
     "revision": "c6b2e506936d419d6d52",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_people-table/precache-manifest.4cccd5f36baaa5cc324f710d4f1d6637.js"
+  "/react_people-table/precache-manifest.75316195a0386e10e0cbb7e7ef9a140a.js"
 );
 
 self.addEventListener('message', (event) => {
